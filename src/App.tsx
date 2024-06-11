6ada4999b00a572295a1ed770ba7b9a64f069546
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-10 21:14:24 -0300

    feat: implements default header/content/footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+
 function App() {
 
   return (
-    <div>
-      vite app
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-1 flex">
+        <div className="container py-8">
+          app content
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
