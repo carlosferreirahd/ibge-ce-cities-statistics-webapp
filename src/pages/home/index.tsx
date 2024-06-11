d12ef2cf911abf3a66230407d42b4f14624259e4
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-11 14:11:24 -0300

    feat: home heading section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,32 @@
+import ceImg from "@/assets/ce.png";
+
 export function Home() {
 
   return (
     <section className="w-full h-full">
-      <h2 className="text-base-content text-2xl font-bold">
-        Apresentador de dados estatísticos do Estado do Ceará
-      </h2>
-      <p className="text-base-content opacity-50 mt-2">
-        Selecione municípios para detalhar as informações
-      </p>
+      <div className="hero place-items-start bottom-line">
+        <div className="hero-content flex-col gap-8 lg:flex-row">
+          <img
+            src={ceImg}
+            className="max-w-sm hidden lg:block"
+            width={180}
+          />
+          <div className="max-w-2xl">
+            <h2 className="text-base-content text-4xl font-bold">
+              Apresentador de {" "}
+              <span
+                className="text-transparent bg-clip-text bg-gradient-to-r to-primary from-info"
+              >
+                dados estatísticos
+              </span>
+              {" "} do Estado do Ceará
+            </h2>
+            <p className="py-3 text-base-content opacity-50 text-xl">
+              Selecione municípios para detalhar as informações.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
