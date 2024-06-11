0ac2fe95512e22028636065c982d659f11defe5d
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-10 21:32:03 -0300

    feat: adding sun and moon icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+// icons from heroicons: https://heroicons.com/
+
+export { SunIcon } from "./sun";
+export { MoonIcon } from "./moon";
+export { ExternalLinkIcon } from "./external-link";
+export { HeartIcon } from "./heart";
