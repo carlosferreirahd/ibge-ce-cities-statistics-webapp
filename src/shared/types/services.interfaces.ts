b510f7d61589f9fce4cc972325d01db1514153b5
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-11 01:26:06 -0300

    feat: defining shared interfaces for services typings

---
+++ src/shared/types/services.interfaces.ts
@@ -0,0 +1,19 @@
+export interface ICity {
+  id: number;
+  nome: string;
+}
+
+export interface IPopulation {
+  ano: number;
+  populacao: number;
+}
+
+export interface ILiteracy {
+  ano: number;
+  taxa: number;
+}
+
+export interface IGDP {
+  ano: number;
+  valor: number;
+}
