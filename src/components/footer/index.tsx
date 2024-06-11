636c9253a528c77a1ca1fc2fa21441e82e007ab3
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-10 21:26:46 -0300

    feat: footer with heart and external link icons

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,36 @@
+import { ExternalLinkIcon, HeartIcon } from "@/components/icons";
+
 export function Footer() {
 
   return (
-    <footer>
-      footer
+    <footer className="w-full bg-base-300 text-base-content z-10">
+      <hr className="divider m-0 border-none h-auto" />
+      <div className="container">
+        <div className="footer footer-center p-4">
+          <div className="text-base font-semibold">
+            <p>
+              Feito com {" "}
+              <HeartIcon
+                className="size-6 inline-block align-middle text-red-600"
+                aria-label="amor"
+              />
+              {" "} por {" "}
+              <a
+                className="link link-hover hover:link-primary"
+                href="https://github.com/carlosferreirahd"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @carlosferreirahd {" "}
+                <ExternalLinkIcon
+                  className="size-4 inline-flex align-baseline"
+                  strokeWidth={2}
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
