f52603d31ca0945d9a0293a18533f5957461a778
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-11 01:25:00 -0300

    feat: configuring react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "@/components/error-boundary";
 import { ThemeProvider } from "@/context/theme";
@@ -5,23 +6,27 @@ import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import Routes from "./routes";
 
+const queryClient = new QueryClient();
+
 function App() {
 
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <ThemeProvider>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            <div className="flex-1 flex">
-              <main className="container py-8">
-                <Routes />
-              </main>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider>
+            <div className="min-h-screen flex flex-col">
+              <Header />
+              <div className="flex-1 flex">
+                <main className="container py-8">
+                  <Routes />
+                </main>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </ThemeProvider>
-      </BrowserRouter>
+          </ThemeProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
