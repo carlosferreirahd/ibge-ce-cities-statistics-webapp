d5efa70299962003c4c09efddaeacf6eab5adf6a
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-12 18:21:58 -0300

    feat: magnify icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -6,3 +6,4 @@ export { ExternalLinkIcon } from "./external-link";
 export { HeartIcon } from "./heart";
 export { XCircleIcon } from "./x-circle";
 export { LocationIcon } from "./location";
+export { MagnifyIcon } from "./magnify";
