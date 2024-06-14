e2db17b84b7a1ffbf2b96fb705955160f06076a6
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-14 19:07:05 -0300

    feat: pagination component with buttons to next and previous pages

--- src/components/cities-list/index.tsx
+++ src/components/cities-list/index.tsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import { useCities } from "@/hooks/use-cities";
 import { useSearch } from "@/hooks/use-search";
-import { EmptyIcon, LocationIcon, MagnifyIcon, XCircleIcon } from "@/components/icons";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EmptyIcon,
+  LocationIcon,
+  MagnifyIcon,
+  XCircleIcon,
+} from "@/components/icons";
 import { ICity } from "@/shared/types/services.interfaces";
 
 export function CitiesList() {
@@ -89,38 +96,63 @@ function List({
   }
 
   return (
-    <ul className="min-w-0 flex flex-row flex-wrap justify-center gap-y-6 -mx-3">
-      {citiesList?.map((city) => (
-        <li
-          key={city.id}
-          className="
+    <div className="flex flex-col gap-10">
+      <ul className="min-w-0 flex flex-row flex-wrap justify-center gap-y-6 -mx-3">
+        {citiesList?.map((city) => (
+          <li
+            key={city.id}
+            className="
               flex-grow-0 flex-shrink-0 px-3 basis-full max-w-full
               sm:basis-1/2 sm:max-w-[50%]
               lg:basis-1/3 lg:max-w-[33.333333%]
               xl:basis-1/4 xl:max-w-[25%]
               2xl:basis-1/5 2xl:max-w-[20%]
             "
-        >
-          <div className="card card-compact bg-base-200 shadow-lg h-full">
-            <div className="card-body justify-between">
-              <h3 className="card-title items-start">
-                <LocationIcon className="size-6 shrink-0 inline-block align-middle" /> {city.nome}
-              </h3>
-              <p>
-                #{city.id}
-              </p>
-              <div className="card-actions justify-end">
-                <Link
-                  to={`/cidade/${city.id}`}
-                  className="btn btn-sm btn-outline btn-primary"
-                >
-                  Detalhes
-                </Link>
+          >
+            <div className="card card-compact bg-base-200 shadow-lg h-full">
+              <div className="card-body justify-between">
+                <h3 className="card-title items-start">
+                  <LocationIcon className="size-6 shrink-0 inline-block align-middle" /> {city.nome}
+                </h3>
+                <p>
+                  #{city.id}
+                </p>
+                <div className="card-actions justify-end">
+                  <Link
+                    to={`/cidade/${city.id}`}
+                    className="btn btn-sm btn-outline btn-primary"
+                  >
+                    Detalhes
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+      <div className="join self-end shadow-lg">
+        <button
+          className="join-item btn"
+          aria-label="Ir para a página anterior"
+        >
+          <ChevronLeftIcon
+            className="size-4"
+            strokeWidth={2}
+          />
+        </button>
+        <div className="join-item btn">
+          Page 22
+        </div>
+        <button
+          className="join-item btn"
+          aria-label="Ir para a próxima página"
+        >
+          <ChevronRightIcon
+            className="size-4"
+            strokeWidth={2}
+          />
+        </button>
+      </div>
+    </div>
   );
 }
