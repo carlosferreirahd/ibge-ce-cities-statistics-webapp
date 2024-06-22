68b71f243b850acfc615c1ca40d31e7710781e15
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-06-22 14:46:32 -0300

    feat: city details page with cities list fetch, error handling and header with city name and id

--- src/pages/city/index.tsx
+++ src/pages/city/index.tsx
@@ -1,12 +1,85 @@
-import { useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FetchError } from "@/components/fetch-error";
+import { ArrowLeftIcon, InfoIcon } from "@/components/icons";
+import { useCities } from "@/hooks/use-cities";
+import { ICity } from "@/shared/types/services.interfaces";
 
 export function City() {
 
   const { cityId } = useParams();
 
+  const {
+    data,
+    isPending,
+    isError,
+    error
+  } = useCities();
+
+  const currentCity = useMemo<ICity | undefined>(
+    () => data?.find(city => city.id == Number(cityId)),
+    [cityId, data]
+  );
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col gap-4 h-full">
+        <div className="skeleton h-10 w-60" />
+        <div className="skeleton h-10 w-40" />
+        <div className="skeleton h-full" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <FetchError
+        title="Erro ao buscar cidades"
+        message={error?.message}
+      />
+    );
+  }
+
+  if (!currentCity) {
+    return (
+      <div role="alert" className="alert alert-info shadow-lg max-w-lg mx-auto">
+        <InfoIcon className="size-12 shrink-0" />
+        <div>
+          <h3 className="font-bold">
+            Cidade não encontrada
+          </h3>
+          <div className="text-xs">
+            Certifique-se de que o <i>ID</i> do município corresponde a um município cearense.
+          </div>
+        </div>
+        <Link to="/cidades" className="btn btn-sm">
+          Voltar
+        </Link>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      cidade {cityId}
-    </div>
+    <section className="w-full h-full">
+      <div className="hero place-items-start">
+        <div className="hero-content flex-row">
+          <Link
+            to="/cidades"
+            className="btn btn-circle btn-ghost self-start -mt-1"
+            aria-label="Link para a página de listagem de cidades"
+          >
+            <ArrowLeftIcon className="size-8" />
+          </Link>
+          <div className="max-w-2xl">
+            <h2 className="text-base-content text-4xl font-bold">
+              {currentCity.nome}
+            </h2>
+            <p className="py-3 text-base-content opacity-50 text-xl">
+              #{currentCity.id}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
